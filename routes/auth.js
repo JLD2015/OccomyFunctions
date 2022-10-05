60e990fcfca805e1219c8821ae9da5169e319746
Jon-Louis Dalton <jldalton12@gmail.com>
2022-10-05 10:54:15 +0200

    Added last of auth API endpoints

--- routes/auth.js
+++ routes/auth.js
@@ -418,5 +418,187 @@ router.post("/deleteaccount", async (req, res) => {
     });
 });
 
+// Update financial details
+router.post("/updatefinancialdetails", async (req, res) => {
+  // Get the auth token
+  if (!req.headers.authorization) {
+    res.status(400);
+    res.json({ status: "Please provide a valid auth token" });
+    return;
+  }
+  const authorization = req.headers.authorization;
+
+  // Get the bank name
+  if (!req.body.bankname) {
+    res.status(400);
+    res.json({ status: "Please provide a bank name" });
+    return;
+  }
+  const bankname = req.body.bankname;
+
+  // Get the bank account number
+  if (!req.body.bankaccountnumber) {
+    res.status(400);
+    res.json({ status: "Please provide a bank account number" });
+    return;
+  }
+  const bankaccountnumber = req.body.bankaccountnumber;
+
+  // Verify the token
+  firebase
+    .auth()
+    .verifyIdToken(authorization)
+    .then(async (decodedToken) => {
+      const uid = decodedToken.uid;
+
+      // Update the user's financial details
+      firebase
+        .firestore()
+        .collection("users")
+        .doc(uid)
+        .update({ bankAccountNumber: bankaccountnumber, bankName: bankname })
+        .then(() => {
+          // Trigger the callback
+          res.status(200);
+          res.json({ status: "Success" });
+          return;
+        });
+    }) // If the ID token could not be verified
+    .catch(() => {
+      res.status(400);
+      res.json({ status: "Invalid token" });
+      return;
+    });
+});
+
+// Update financial details
+router.post("/updateprofiledetails", async (req, res) => {
+  // Get the auth token
+  if (!req.headers.authorization) {
+    res.status(400);
+    res.json({ status: "Please provide a valid auth token" });
+    return;
+  }
+  const authorization = req.headers.authorization;
+
+  const form = new formidable.IncomingForm();
+  form.parse(req, async (err, fields, files) => {
+    // If there was an error retrieving the form
+    if (err) {
+      res.status(400);
+      res.json({ status: "Something went wrong" });
+      return;
+    }
+
+    // Get the profile picture
+    if (!files.profilepicture) {
+      res.status(400);
+      res.json({ status: "Please provide a profile picture" });
+      return;
+    }
+    const profilepicturepath = files.profilepicture.filepath;
+
+    // Retrieve name
+    if (!fields.name) {
+      res.status(400);
+      res.json({ status: "Please provide a name" });
+      return;
+    }
+    const name = fields.name;
+
+    // Rerieve phone number
+    if (!fields.phone) {
+      res.status(400);
+      res.json({ status: "Please provide a phone number" });
+      return;
+    }
+    const phone = fields.phone;
+
+    // Retrieve jid
+    if (!fields.jid) {
+      res.status(400);
+      res.json({ status: "Please provide a jid" });
+      return;
+    }
+    const jidfull = fields.jid;
+    const jid = fields.jid.split("@")[0];
+
+    // Retrieve jid password
+    if (!fields.jidpassword) {
+      res.status(400);
+      res.json({ status: "Please provide a jid password" });
+      return;
+    }
+    const jidpassword = fields.jidpassword;
+
+    // Verify the token
+    firebase
+      .auth()
+      .verifyIdToken(authorization)
+      .then(async (decodedToken) => {
+        const uid = decodedToken.uid;
+
+        // Start transaction
+        try {
+          firebase.firestore().runTransaction(async (transaction) => {
+            // Encode the user's profile picture
+            const base64Image = fs.readFileSync(profilepicturepath, "base64");
+
+            // Update the user's details
+            firebase.firestore().collection("users").doc(uid).update({
+              name: name,
+              phoneNumber: phone,
+              profilePhoto: base64Image,
+            });
+
+            // 6. Update the user's XMPP VCard
+            const xmpp = client({
+              service: "xmpp://xmpp.occomy.com:5222",
+              username: jid,
+              password: jidpassword,
+            });
+
+            // Monitor when we go online
+            xmpp.on("online", async (address) => {
+              const iq = xml(
+                "iq",
+                { id: uuid(), type: "set" },
+                xml(
+                  "vCard",
+                  { xmlns: "vcard-temp" },
+                  xml("FN", {}, name),
+                  xml("JABBERID", {}, jidfull),
+                  xml(
+                    "PHOTO",
+                    {},
+                    xml("TYPE", {}, "image/jpeg"),
+                    xml("BINVAL", {}, base64Image)
+                  )
+                )
+              );
+              await xmpp.send(iq);
+              res.status(200);
+              res.json({ status: "Success" });
+              return;
+            });
+
+            // Fourth we start the XMPP connection
+            xmpp.start().catch(console.error);
+          });
+        } catch (error) {
+          console.log(error);
+          res.status(400);
+          res.json({ status: "Failed" });
+          return;
+        }
+      }) // If the ID token could not be verified
+      .catch(() => {
+        res.status(400);
+        res.json({ status: "Invalid token" });
+        return;
+      });
+  });
+});
+
 // Export the router
 module.exports = router;
